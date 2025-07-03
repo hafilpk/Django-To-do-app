0ad9c63cd81f94ae7eaec0f66e084f1be088d0ec
hafilpk <pkmshafil@gmail.com>
2025-07-03 16:35:17 +0530

    update and delete function added

--- todo/views.py
+++ todo/views.py
@@ -8,12 +8,27 @@ def index(request):
 
     if request.method =="POST":
         form = TaskForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
         return redirect('/')    
     context = {"tasks": tasks, "TaskForm": form}
-
     return render(request, 'index.html', context)
 
 def update(request, pk):
     task = Task.objects.get(id = pk)
+    form = TaskForm(instance=task)
+    if request.method =="POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {"TaskForm": form}
+    return render(request, 'update.html', context)
+
+def delete(request, pk):
+    task = Task.objects.get(id = pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    context = {"task": task}
+    return render(request, 'delete.html', context)    
